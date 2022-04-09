b30f26208f708559e0283470b4a3817de40ce549
Taku Matsunaga <corvettez06511@gmail.com>
2022-04-09 10:09:47 +0900

    create list

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  FlatList,
+  SafeAreaView,
+} from 'react-native';
+import ListItem from './components/ListItem';
+import dummyArticles from './dummies/articles.json';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    console.log(URL);
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+      try {
+        const response = await axios.get(URL);
+        setArticles(response.data.articles)
+        // console.log(response);
+      } catch (error) {
+        // console.error(error);
+      }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>ここに文章が入ります！</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+            keyExtractor={(index) => index}
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +55,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });

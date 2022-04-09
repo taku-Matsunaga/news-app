f1ab7a20a679acb7f84f15b7943adca422e566e6
Taku Matsunaga <corvettez06511@gmail.com>
2022-04-09 20:08:48 +0900

    create persist

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from './navigation/AppNavigator';
 import { Provider } from 'react-redux';
-import store from './store';
+import store, { persistor } from './store';
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <AppNavigator>
-        <StatusBar style="auto" />
-      </AppNavigator>
+      <PersistGate loading={null} persistor={persistor}>
+        <AppNavigator>
+          <StatusBar style="auto" />
+        </AppNavigator>
+      </PersistGate>
     </Provider>
   );
 }

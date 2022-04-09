0c55e15ebbf80bd3b938f1ca5aff70cd34518ffa
Taku Matsunaga <corvettez06511@gmail.com>
2022-04-09 22:07:44 +0900

    complete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  RefreshControl,
+} from 'react-native';
 import Constants from 'expo-constants';
 import axios from 'axios';
 import ListItem from '../components/ListItem';
@@ -12,21 +17,48 @@ export default function HomeScreen(props) {
   const { navigation } = props;
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const pageRef = useRef(1);
+  const fetchedAllRef = useRef(false);
 
   useEffect(() => {
-    fetchArticles();
+    setLoading(true);
+    fetchArticles(1);
   }, []);
 
-  const fetchArticles = async () => {
-    setLoading(true);
+  const fetchArticles = async (page) => {
     try {
-      const response = await axios.get(URL);
-      setArticles(response.data.articles);
+      const response = await axios.get(`${URL}&page=${page}`);
+      if (response.data.articles.length > 0) {
+        // setArticles(response.data.articles);
+        setArticles((prevArticles) => [
+          ...prevArticles,
+          ...response.data.articles,
+        ]);
+      } else {
+        fetchedAllRef.current = true;
+      }
       // console.log(response);
     } catch (error) {
       // console.error(error);
     }
     setLoading(false);
+  };
+
+  const onEndReached = () => {
+    if (!fetchedAllRef.current) {
+      pageRef.current = pageRef.current + 1;
+      fetchArticles(pageRef.current);
+    }
+  };
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    setArticles([]);
+    pageRef.current = 1;
+    fetchedAllRef.current = false;
+    await fetchArticles(1);
+    setRefreshing(false);
   };
 
   return (
@@ -46,6 +78,10 @@ export default function HomeScreen(props) {
             }
           />
         )}
+        onEndReached={onEndReached}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
       {loading && <Loading />}
     </SafeAreaView>
